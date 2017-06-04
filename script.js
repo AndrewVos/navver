8b323be738a63fa7d52114a6296eaf0eac56cce8
Andrew Vos <andrew.vos@gmail.com>
2017-06-04 15:20:38 +0100

    Switch to easymotion-like navigation

--- script.js
+++ script.js
@@ -1,70 +1,109 @@
 document.onkeydown = keyboardNavigation
 
 function keyboardNavigation(e) {
-  if (e.code == 'KeyG') {
-    console.log("Navver enabled")
+  if (shouldLaunchNavver(e)) {
+    createTags()
+    return false
+  }
 
-    window.navverEnabled = true
-    window.navverRectangle = null
-    renderRectangle()
-  } else if (e.code == 'Escape') {
-    console.log("Navver disabled")
+  if (window.navverTags) {
+    processKeyboardEvent(e)
+  }
+}
 
-    window.navverEnabled = false
-    if (window.navverRectangle) {
-      window.navverRectangle.remove()
+function processKeyboardEvent(e) {
+  window.navverKeysPressed += e.key
+  search(window.navverKeysPressed)
+  e.preventDefault()
+  return false
+}
+
+function shouldLaunchNavver(e) {
+  return e.code == 'KeyG' &&
+    e.srcElement.tagName != 'INPUT'
+    !window.navverTags
+}
+
+function search(search) {
+  for (var i = 0; i < window.navverTags.length; i++) {
+    var tag = window.navverTags[i]
+
+    if (tag.label == search) {
+      removeAllTags()
+      tag.click()
+      return
+    } else if (tag.label[0] == search) {
+      tag.highlight(search)
+    } else {
+      tag.remove()
+      window.navverTags.splice(i, 1)
+      i--
     }
-    window.navverRectangle = null
   }
 
-  if (!window.navverEnabled) {
-    return
+  if (window.navverTags.length == 0) {
+    removeAllTags()
   }
 
-  if (e.code == 'ArrowUp') {
-    up()
-    return false
-  } else if (e.code == 'ArrowDown') {
-    down()
-    return false
-  } else if (e.code == 'ArrowLeft') {
-    left()
-    return false
-  } else if (e.code == 'ArrowRight') {
-    right()
-    return false
+  return null
+}
+
+function removeAllTags () {
+  for (var i = 0; i < window.navverTags.length; i++) {
+    var tag = window.navverTags[i]
+    tag.remove()
   }
+  window.navverTags = null
 }
 
-function up () {
-  window.navverRectangle.css('height', window.navverRectangle.height() / 2)
+function createTags () {
+  window.navverKeysPressed = ''
+  window.navverTags = []
+
+  var labelIndex = 0
+  var labels = generateLabels()
+
+  $('a, button').each(function () {
+    var label = labels[labelIndex]
+    var $a = $(this)
+    var $tag = $('<div class="navver-tag">' + label+ '</div>')
+    $tag.css('position', 'absolute')
+    $tag.css('top', $a.offset().top)
+    $tag.css('left', $a.offset().left)
+    $tag.css('background', 'yellow')
+    $tag.css('border', '1px solid #333')
+    $tag.css('border-radius', '3px')
+    $tag.css('z-index', 10000000000)
+    $('body').append($tag)
+    window.navverTags.push(
+      {
+        label: label,
+        remove: function () {
+          $tag.remove()
+        },
+        click: function () {
+          $a[0].click()
+        },
+        highlight: function (search) {
+          if (search == label) {
+            $tag.html("<strong>" + label + "</strong>")
+          } else if (search == label[0]) {
+            $tag.html("<strong>" + label[0] + "</strong>" + label[1])
+          }
+        }
+      }
+    )
+    labelIndex += 1
+  })
 }
 
-function down () {
-  window.navverRectangle.css('top', window.navverRectangle.offset().top + (window.navverRectangle.height() / 2))
-  window.navverRectangle.css('height', window.navverRectangle.height() / 2)
-}
-
-function left () {
-  window.navverRectangle.css('width', window.navverRectangle.width() / 2)
-}
-
-function right () {
-  window.navverRectangle.css('left', window.navverRectangle.offset().left + (window.navverRectangle.width() / 2))
-  window.navverRectangle.css('width', window.navverRectangle.width() / 2)
-}
-
-function renderRectangle () {
-  if (!window.navverRectangle) {
-    window.navverRectangle = $('<div class="navver"></div>')
-    window.navverRectangle.css('position', 'absolute')
-    window.navverRectangle.css('background', 'rgba(255,0,0,0.1)')
-    window.navverRectangle.css('pointer-events', 'none')
-    window.navverRectangle.css('top', $(window).scrollTop())
-    window.navverRectangle.css('left', $(window).scrollLeft())
-
-    window.navverRectangle.css('width', window.innerWidth)
-    window.navverRectangle.css('height', window.innerHeight)
-    $('body').append(window.navverRectangle)
+function generateLabels () {
+  var characters = 'abcdefghijklmnopqrstuvwxyz'
+  var labels = []
+  for (var i = 0; i < characters.length; i++) {
+    for (var p = 0; p < characters.length; p++) {
+      labels.push(characters[i] + characters[p])
+    }
   }
+  return labels
 }
